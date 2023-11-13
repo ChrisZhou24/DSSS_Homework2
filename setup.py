cdc7de5f385a95751a6935a7a70576842b0c7dd5
ChrisZhou24 <149950073+ChrisZhou24@users.noreply.github.com>
2023-11-13 19:01:39 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DSSS_Homework2',
+    version='1.2.0',
+    packages=[''],
+    url='https://github.com/ChrisZhou24/DSSS_Homework2',
+    license='',
+    author='zhouzixuan',
+    author_email='',
+    description=''
+)
